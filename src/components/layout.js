77eec7184b53d0357c480b3042f82ea95e3132fa
Grace Jiang <gracjiang@gmail.com>
2022-01-03 07:12:11 -0800

    build site

---
+++ src/components/layout.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import FavIcon from "../images/favicon.png"
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <html lang="en">
+        <head>
+          <title>{this.props.title}</title>
+          <link rel="icon" type="image/png" href={FavIcon} />
+        </head>
+        <body>{this.props.children}</body>
+      </html>
+    );
+  }
+}
+
+export default Layout;
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+
+const IndexPage = () => {
+  return (
+    <Layout title="Spark Community">
+      <h1>Community!</h1>
+    </Layout>
+  )
+}
+
+export default IndexPage
